0f1c151dfa9dd3cd09ad43502f267e2e029a1af6
Panzeroishi <72875007+Panzeroishi@users.noreply.github.com>
2021-03-31 17:41:47 -0300

    Adicionado arquivos finais

---
+++ script1.js
@@ -0,0 +1,27 @@
+function sobre1(){
+    document.getElementById("resultado").innerHTML= "Descrição Sobre Inverno"
+}
+function sair(){
+    document.getElementById("resultado").innerHTML=""
+}
+
+function sobre2(){
+    document.getElementById("resultado2").innerHTML="Descrição Sobre Outono"
+}
+function sair2(){
+    document.getElementById("resultado2").innerHTML=""
+}
+
+function sobre3(){
+    document.getElementById("resultado3").innerHTML="Descrição Sobre Primavera"
+}
+function sair3(){
+    document.getElementById("resultado3").innerHTML=""
+}
+
+function sobre4(){
+    document.getElementById("resultado4").innerHTML="Descrição Sobre Inverno"
+}
+function sair4(){
+    document.getElementById("resultado4").innerHTML=""
+}
